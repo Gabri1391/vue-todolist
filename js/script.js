f2fa11b4051cebd0fdcfd962c00b0f70de983da2
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-13 16:00:32 +0200

    Milestone 1 done

--- js/script.js
+++ js/script.js
@@ -4,7 +4,29 @@ const root = new Vue({
     el: '#root',
     data: {
         tasks: [
-            {}
+            {text: 'Dare da mangiare al cane',
+             done: true,
+            },
+
+            {text: 'Fare aggiornamento a Windows',
+             done: false,
+            },
+
+            {text: 'Fare la spesa',
+             done: true,
+            },
+
+            {text: 'Collegarsi su Zoom per il meeting aziendale',
+             done: false,
+            },
+
+            {text: 'Portare fuori la spazzatura',
+             done: true,
+            }
         ]
+    },
+
+    methods: {
+        
     }
 });
